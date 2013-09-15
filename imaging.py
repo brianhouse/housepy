02a02fef347be1e0d2d6ce6128fd4555f83c6d72
brianhouse <brian.house@gmail.com>
2013-09-15 19:14:37 -0400

    taking out opencv functions

--- imaging.py
+++ imaging.py
@@ -1,29 +1,6 @@
 from PIL import Image
 from .log import log
 
-def ipl_to_pil(cv_image):
-    """Convert an ipl (OpenCV) image to PIL"""
-    import cv    
-    pil_image = Image.fromstring("RGB", cv.GetSize(cv_image), cv_image.tostring(), "raw", "BGR")
-    return pil_image
-    
-def pil_to_ipl(pil_image):
-    """Convert a PIL image to ipl (OpenCV)"""    
-    import cv
-    cv_image = cv.CreateImageHeader(pil_image.size, cv.IPL_DEPTH_8U, 3)
-    cv.SetData(cv_image, pil_image.rotate(180).tostring()[::-1])
-    return cv_image
-    
-def numpy_to_ipl(data, channels=1):
-    """Convert a NumPy image to ipl (OpenCV) with three channels"""    
-    import cv    
-    if channels == 1:
-        cv_image = cv.CreateImageHeader((data.shape[1], data.shape[0]), cv.IPL_DEPTH_16U, 1)
-        cv.SetData(cv_image, data.tostring(), data.dtype.itemsize * data.shape[1])
-    elif channels == 3:
-        cv_image = cv.CreateImageHeader((data.shape[1], data.shape[0]), cv.IPL_DEPTH_8U, 3)
-        cv.SetData(cv_image, data[:, :, ::-1].tostring(), data.dtype.itemsize * 3 * data.shape[1])     # we swap from RGB to BGR here
-    return cv_image
     
 def resize(image, size, exact=True):
     """Exact mode crops to create the desired aspect ratio. Otherwise, an image of the maximum size will be returned that fits in the bounding box. PIL."""
