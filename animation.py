02a2cb2f14d7e02ebb1a8268bfba515a461f5de3
brianhouse <brian.house@gmail.com>
2016-01-19 20:10:49 -0500

    adapted for 3d

--- animation.py
+++ animation.py
@@ -54,17 +54,27 @@ class Context(dispatcher.Dispatcher):
             self.window.set_location(screen.x, screen.y)
         self.window.on_mouse_press = self.on_mouse_press
         self.window.on_mouse_release = self.on_mouse_release
+        self.window.on_mouse_drag = self.on_mouse_drag
         self.draw_func = draw_func
         self.update_func = update_func if update_func is not None else lambda x: x        
         pyglet.gl.glClearColor(*self._background)        
         if self._3d:
-            pyglet.gl.glColor3f(1, 0, 0)
+            pyglet.gl.glViewport(0, 0, self.width, self.height)
+            pyglet.gl.glMatrixMode(pyglet.gl.GL_PROJECTION)
+            pyglet.gl.glLoadIdentity()
+            # pyglet.gl.gluPerspective(60.0, self.width / self.height, 0.1, 1000.0) # on-axis projection
+            # usr Frustum to do off-axis projection (if the coords arent symmetrical). Equiv to gluPerspective above is left, right, bottom, top = -0.082479, 0.082479, -0.057735, 0.057735
+            left, right, bottom, top = -0.082479, 0.082479, -0.057735, 0.057735
+            # left, right, bottom, top = -0.001, 0.001, -0.001, 0.001
+            pyglet.gl.glFrustum(left, right, bottom, top, 0.1, 1000.0) 
+            pyglet.gl.glMatrixMode(pyglet.gl.GL_MODELVIEW)
             pyglet.gl.glEnable(pyglet.gl.GL_DEPTH_TEST)
-            pyglet.gl.glEnable(pyglet.gl.GL_CULL_FACE)
+            # for 3d shapes / shading
+            # pyglet.gl.glEnable(pyglet.gl.GL_CULL_FACE)
             # pyglet.gl.glPolygonMode(pyglet.gl.GL_FRONT_AND_BACK, pyglet.gl.GL_LINE) # Uncomment this line for a wireframe view    
-            pyglet.gl.glEnable(pyglet.gl.GL_LIGHTING)
-            pyglet.gl.glEnable(pyglet.gl.GL_LIGHT0)
-            pyglet.gl.glEnable(pyglet.gl.GL_LIGHT1)
+            # pyglet.gl.glEnable(pyglet.gl.GL_LIGHTING)
+            # pyglet.gl.glEnable(pyglet.gl.GL_LIGHT0)
+            # pyglet.gl.glEnable(pyglet.gl.GL_LIGHT1)
         if self.smooth:
             pyglet.gl.glBlendFunc(pyglet.gl.GL_SRC_ALPHA, pyglet.gl.GL_ONE_MINUS_SRC_ALPHA)                             
             pyglet.gl.glEnable(pyglet.gl.GL_BLEND)                                                            
@@ -72,21 +82,18 @@ class Context(dispatcher.Dispatcher):
             pyglet.gl.glHint(pyglet.gl.GL_LINE_SMOOTH_HINT, pyglet.gl.GL_NICEST)    
         self.window.on_draw = self.draw_loop
         if self._3d:
-            self.window.on_resize = self.on_resize
+            self.window.on_resize = lambda w,h: True    # stop the default 2d projection
         pyglet.clock.schedule(self.update_func)
         pyglet.app.run()            
 
-    def on_resize(self, width, height):
-        if self._3d:
-            pyglet.gl.glViewport(0, 0, width, height)
-            pyglet.gl.glMatrixMode(pyglet.gl.GL_PROJECTION)
-            pyglet.gl.glLoadIdentity()
-            pyglet.gl.gluPerspective(60.0, width / height, 0.1, 1000.0)
-            pyglet.gl.glMatrixMode(pyglet.gl.GL_MODELVIEW)
-        return pyglet.event.EVENT_HANDLED
+    def translate(self, *matrix):
+        pyglet.gl.glTranslatef(*matrix)
+
+    def rotate(self, *matrix):
+        pyglet.gl.glRotatef(*matrix)
 
     def draw_loop(self):
-        self.window.clear()
+        self.window.clear()        
         pyglet.gl.glLoadIdentity()        
         self.draw_func()
         for o in self.objects:
@@ -98,23 +105,37 @@ class Context(dispatcher.Dispatcher):
     def line(self, x1, y1, x2, y2, color=(0., 0., 0., 1.), thickness=1.0):
         pyglet.gl.glColor4f(*color)    
         pyglet.gl.glLineWidth(thickness) 
-        pyglet.graphics.draw(2, pyglet.gl.GL_LINES,
-            ('v2f', (x1 * self.width, y1 * self.height, x2 * self.width, y2 * self.height))
-        )        
+        if not self._3d:
+            pyglet.graphics.draw(2, pyglet.gl.GL_LINES,
+                ('v2f', (x1 * self.width, y1 * self.height, x2 * self.width, y2 * self.height))
+            )        
+        else:
+            pyglet.graphics.draw(2, pyglet.gl.GL_LINES,
+                ('v2f', (x1, y1, x2, y2))
+            )                    
 
-    def line3(self, x1, y1, z1, x2, y2, z2, color=(0., 0., 0., 1.), thickness=1.0):
+    def line3D(self, x1, y1, z1, x2, y2, z2, color=(0., 0., 0., 1.), thickness=1.0):
         self.depth = self.height
         pyglet.gl.glColor4f(*color)    
         pyglet.gl.glLineWidth(thickness) 
         pyglet.graphics.draw(2, pyglet.gl.GL_LINES,
-            ('v3f', (x1 * self.width, y1 * self.height, z1 * self.depth, x2 * self.width, y2 * self.height, z2 * self.depth))
+            ('v3f', (x1, y1, z1, x2, y2, z2))   # dont multiply by canvas -- but by what?
         )  
 
     def lines(self, points, color=(0., 0., 0., 1.), thickness=1.0):
         pyglet.gl.glColor4f(*color)    
-        pyglet.gl.glLineWidth(thickness) 
-        points = [(item * self.width) if (i % 2 == 0) else (item * self.height) for sublist in points for (i, item) in enumerate(sublist)] # flatten
+        pyglet.gl.glLineWidth(thickness)
+        if not self._3d:
+            points = [(item * self.width) if (i % 2 == 0) else (item * self.height) for sublist in points for (i, item) in enumerate(sublist)] # flatten
+        else:
+            points = [item for sublist in points for (i, item) in enumerate(sublist)]
         pyglet.graphics.draw(len(points) // 2, pyglet.gl.GL_LINE_STRIP, ('v2f', points))        
+
+    def lines3D(self, points, color=(0., 0., 0., 1.), thickness=1.0):
+        pyglet.gl.glColor4f(*color)    
+        pyglet.gl.glLineWidth(thickness) 
+        points = [item for sublist in points for (i, item) in enumerate(sublist)] # flatten
+        pyglet.graphics.draw(len(points) // 3, pyglet.gl.GL_LINE_STRIP, ('v3f', points))        
 
     def plot(self, signal, color=(0., 0., 0., 1.), thickness=1.0):
         points = [(float(s) / self.width, sample) for (s, sample) in enumerate(signal)]
@@ -123,10 +144,11 @@ class Context(dispatcher.Dispatcher):
     def rect(self, x, y, width, height, color=(0., 0., 0., 1.), thickness=1.0):
         pyglet.gl.glColor4f(*color)    
         pyglet.gl.glLineWidth(thickness) 
-        x *= self.width
-        y *= self.height
-        width *= self.width
-        height *= self.height
+        if not self._3d:
+            x *= self.width
+            y *= self.height
+            width *= self.width
+            height *= self.height
         pyglet.graphics.draw(4, pyglet.gl.GL_QUADS,
             ('v2f', (x, y, x, y + height, x + width, y + height, x + width, y)),
             # ('c3b', (color, color, color, color))
@@ -152,10 +174,24 @@ class Context(dispatcher.Dispatcher):
         return l
 
     def on_mouse_press(self, x, y, button, modifiers):
-        self.fire('mouse_press', (x/self.width, y/self.width, button, modifiers))
+        if not self._3d:
+            x /= self.width
+            y /= self.height
+        self.fire('mouse_press', (x, y, button, modifiers))
 
     def on_mouse_release(self, x, y, button, modifiers):
-        self.fire('mouse_release', (x/self.width, y/self.width, button, modifiers))
+        if not self._3d:
+            x /= self.width
+            y /= self.height
+        self.fire('mouse_release', (x, y, button, modifiers))
+
+    def on_mouse_drag(self, x, y, dx, dy, button, modifiers):
+        if not self._3d:
+            x /= self.width
+            y /= self.height
+            dx /= self.width
+            dy /= self.height
+        self.fire('mouse_drag', (x, y, dx, dy, button, modifiers))
 
 
 def rgb_to_html(rgb_tuple):
